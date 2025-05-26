772a97d9d9b7b32ec218a0be13bcf9dce61a0a94
Mykyta_Rymar <mykitarymar@gmail.com>
2025-05-27 00:44:39 +0200

    1 ne gimno

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,8 @@
 function slugify(title) {
-     slugify(title, {
-  replacement: '-',  // replace spaces with replacement character, defaults to `-`
-  remove: undefined, // remove characters that match regex, defaults to `undefined`
-  lower: true,      // convert to lower case, defaults to `false`
-  strict: false,     // strip special characters except replacement, defaults to `false`
-  locale: 'uk',      // language code of the locale to use
-  trim: true         // trim leading and trailing replacement chars, defaults to `true`
-});
+     const popa = title.toLowerCase();
+     const pipa = popa.split(" ");
+     const papa = pipa.join("-");
+     return papa;
 }
 
 console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
